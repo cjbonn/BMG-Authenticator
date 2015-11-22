cab67cc1232efa132c6c08b705c46a5fa8d4f840
Christopher Bonnell <cjbonn2005@hotmail.com>
2015-11-22 01:45:20 -0500

    Final

--- www/js/Auth.js
+++ www/js/Auth.js
@@ -5,7 +5,7 @@
 		var currentFrame = '#loginFrame';
 		var lastFrame = '#loginFrame';
 		var server = "http://bmg.x10host.com/";
-		var currentVersion = "0.9";
+		var currentVersion = "1.0";
 		var frameWidth = 0;
 
 		init();
@@ -23,61 +23,44 @@
 				dataType: "jsonp",
 				timeout: 3000,
 				success: function(data){
-					if(data !== false){
-						if(data[0] == "error"){
-							console.log('error: '+data[1]);
-						}else{
-							if(callback && {}.toString.call(callback) === '[object Function]'){
-								callback(data);
-							}else{
-								console.log("Result: "+data[0]);
-							}
-						}
+					if(callback && {}.toString.call(callback) === '[object Function]'){
+						callback(data);
+					}else{
+						console.log("Result: "+data[0]);
 					}
 				},
 				error: function(x, t, m){
 					console.log(x);
+					$('#loader span').html("Looking for network...")
 				},
 				retryMax: 5
 			});
 		}
 
-		this.changeFrame = function(elem, instant){
-			if(elem == 'last') elem = lastFrame;
-			var pos = $('.frame').index($(elem)) * frameWidth;
-			if(elem == currentFrame || instant == true){
-				$('#frameWrapper').css('marginLeft','-'+pos+'px');
-			}else{
-				$('#frameWrapper').animate({marginLeft:'-'+pos+'px'},500,function(){
-					$(window).trigger('framechanged',[elem]);
-				});
-			}
-			lastFrame = currentFrame;
-			currentFrame = elem;
-		}
-
-		this.isLoggedIn = function(callback){
+		this.checkDevice = function(uid,callback){
 			var result = false;
-			_self.call("isLoggedIn.php",false,function(data){
-				if(data[0] == "bad"){
-					result = false;
-				}else{
-					result = true;
-				}
+			_self.call("checkDevice.php",uid,function(data){
 				if(callback && {}.toString.call(callback) === '[object Function]'){
-					callback(result);
+					callback(data);
 				}
 			});
 		}
 
-		this.refreshScreen = function(){
-			var h = $(window).height();
-			var w = $(window).width();
-			frameWidth = w;
-			$('#frameWrapper').css('width',$('#frameWrapper td').length*w+'px');
-			$('#frameWrapper tr').css({height:h});
-			$('#frameWrapper td').css({height:h,width:w});
-			_self.changeFrame(currentFrame);
+		this.registerDevice = function(uid,user,pin,callback){
+			var args = [uid,user,pin];
+			_self.call("registerDevice.php",args,function(data){
+				if(callback && {}.toString.call(callback) === '[object Function]'){
+					callback(data);
+				}
+			});
+		}
+
+		this.renewKey = function(iid,callback){
+			_self.call("renewKey.php",iid,function(data){
+				if(callback && {}.toString.call(callback) === '[object Function]'){
+					callback(data);
+				}
+			});
 		}
 
 		this.updateVersion = function(){
--- www/js/main.js
+++ www/js/main.js
@@ -3,6 +3,7 @@ $.ajaxPrefilter(function( options, originalOptions, jqXHR ) {
 	if( !originalOptions.timeout >0 ) return;
 	if( originalOptions.retryCount ) {
 		originalOptions.retryCount++;
+		if(originalOptions.retryCount == originalOptions.retryMax) $('#loader').html("Connection Failed.");
 	}else{
 		originalOptions.retryCount = 1;
 		originalOptions._error = originalOptions.error;
@@ -17,13 +18,111 @@ $.ajaxPrefilter(function( options, originalOptions, jqXHR ) {
 	}
 });
 
-$(document).on('deviceready ready',function(){
-	//var uuid = device.uuid;
-	var uuid = 1234;
+// $(document).ready(function(){
+// 	// For browser testing
+// 	if(typeof device == 'undefined'){
+// 		$(document).trigger('deviceready');
+// 	}
+// });
+
+$(document).on('deviceready',function(){
+	window.isSubmitting = false;
+	window.uuid = 1234;
+	if(typeof device !== 'undefined'){
+		window.uuid = device.uuid;
+	}
 	// INIT
-	Auth.refreshScreen();
 	Auth.updateVersion();
-	// Auth.checkDevice();
+	var loader = $('#loader');
+	var loaderContent = $('#loader span');
+	loaderContent.html("Checking Device...")
+	Auth.checkDevice(window.uuid,function(data){
+		if(typeof data === 'undefined' || data == false){
+			loader.html("Invalid server response.");
+		}else if(data[0] == "error"){
+			loader.html(data[1]);
+		}else if(data[0] == "404"){
+			loader.hide();
+			$('#content').fadeIn(400).animate({'height': '400px'},'slow','easeOutCubic', function(){
+				$('#newUserContent').fadeIn('fast');
+			});
+		}else{
+			loader.hide();
+			window.user_id = data[0].id;
+			$('#userFill').html(data[0].username);
+			$('#content').fadeIn(400).animate({'height': '400px'},'slow','easeOutCubic', function(){
+				$('#keyContent').fadeIn('fast',function(){
+					newKey();					
+				});
+			});
+		}
+	});
+
+	function keyTimer(){
+		var t = setInterval(function(){
+			var num = $('#seconds').html();
+			var newNum = parseInt(num);
+			newNum--;
+			$('#seconds').html(newNum);
+			console.log(newNum);
+			if(newNum < 0){
+				newKey();
+				$('#seconds').html(20);
+				clearInterval(t);
+			}
+		},1000);
+	}
+
+	function newKey(){
+		$('#key').val('');
+		$('#key').attr('style','background: rgba(255,255,255,0.2) url("./images/loadingCircle.gif") no-repeat center center / 20px 20px');
+		Auth.renewKey(window.user_id,function(d){
+			if(typeof d === 'undefined' || d == false){
+				$('#key').val("Invalid server response.");
+			}else if(d[0] == "error"){
+				$('#key').val(d[1]);
+			}else{
+				$('#key').val(d[0]);
+			}
+			$('#key').css('background','');
+			keyTimer();
+		});
+	}
+
+	$('#newUserSubmit').click(function(){
+		if(window.isSubmitting == true) return;
+		window.isSubmitting = true;
+		var u = $('#username').val();
+		var p = $('#pin').val();
+		if(p < 0 || p > 9999){
+			$('#pin').css('borderColor','#990000');
+			return;
+		}
+		$('#newUserContent').fadeOut('fast');
+		$('#content').animate({'height':'3px'},'slow','easeOutCubic', function(){
+			$(this).fadeOut(400, function(){
+				loader.show();
+				loaderContent.html('Registering Device...');
+			});
+		});
+		Auth.registerDevice(window.uuid,u,p,function(data){
+			window.isSubmitting = false;
+			if(typeof data === 'undefined' || data == false){
+				loader.html("Invalid server response.");
+			}else if(data[0] == "error"){
+				loader.html(data[1]+" App restarting in 3s.");
+				setTimeout(function(){
+					document.location.reload(true);
+				},5000);
+			}else{
+				loader.html("Device registered. App restarting in 3s.");
+				setTimeout(function(){
+					document.location.reload(true);
+				},5000);
+			}
+
+		});
+	});
 	// // LOGIN
 	// $('#loginError').html("<img src='./images/loadingCircle.gif' style='width: 20px;height:20px;vertical-align:bottom;' />");
 	// $('#loginSubmit,#user,#pass').attr('disabled','disabled');
